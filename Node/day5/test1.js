f01f307bcc46a02104901a9b259fb631301d50be
MadHat13 <1124298082@qq.com>
2019-11-24 09:04:17 +0800

    2019_11_24

--- Node/day5/test1.js
+++ Node/day5/test1.js
@@ -1,4 +1,11 @@
 var http=require('http');
+var url=require('url');
+var fs=require('fs');
 http.createServer((req, res)=>{
-    
+    var pathname=url.parse(req.url).pathname;
+    var realPath=__dirname+'/static'+pathname;
+    var pathname=__dirname+'/static/'+url.parse('index.html').pathname;
+	var indexPage=fs.readFileSync(pathname,"utf-8");
+	res.writeHead(200,{"Content-type":"text/html"});
+	res.end(indexPage);
 }).listen(3000)
